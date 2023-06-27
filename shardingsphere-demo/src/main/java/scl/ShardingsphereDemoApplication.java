00804da7ac7543917a9b33eab2f75ba894150c7e
sichaolong <2589165806@qq.com>
2023-06-27 21:22:27 +0800

    init shardingsphere-demo

--- shardingsphere-demo/src/main/java/scl/ShardingsphereDemoApplication.java
+++ shardingsphere-demo/src/main/java/scl/ShardingsphereDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShardingsphereDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShardingsphereDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingsphereDemoApplication.class, args);
+    }
 
 }
