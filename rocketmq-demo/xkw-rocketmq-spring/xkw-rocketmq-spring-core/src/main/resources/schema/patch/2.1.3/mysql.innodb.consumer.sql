488fa52dc64f96ea9859f3084aea1cc711df0eb5
sichaolong <sichaolong@xkw.cn>
2023-10-19 15:45:39 +0800

    fix git files

--- rocketmq-demo/xkw-rocketmq-spring/xkw-rocketmq-spring-core/src/main/resources/schema/patch/2.1.3/mysql.innodb.consumer.sql
+++
@@ -1,5 +0,0 @@
--- 2.1.3对key的生成规则做了优化
--- 建议应用升级前执行
-alter table xkw_consumer_message
-    modify unique_key varchar(100) default '' null comment '生产者为消息设置的唯一标识';
-
--- rocketmq-demo/xkw-rocketmq-spring/xkw-rocketmq-spring-core/src/main/resources/schema/patch/2.2.3/mysql.innodb.producer.sql
+++
@@ -1,5 +0,0 @@
--- 2.2.3优化了顺序消息的发送逻辑
--- 建议应用升级前执行
-create index `idx_type_hash_key`
-    on xkw_producer_message (`type`, `hash_key`);
-
