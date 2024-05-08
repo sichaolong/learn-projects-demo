18ca1663fba6f012b0b67425185409e219ba3d18
sichaolong <sichaolong@xkw.cn>
2024-05-08 10:12:27 +0800

    feat：init spring-ai module

---
+++ llm-demo/springai-demo/src/main/java/scl/springaidemo/SpringaiDemoApplication.java
@@ -0,0 +1,13 @@
+package scl.springaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiDemoApplication.class, args);
+    }
+
+}
---
+++ llm-demo/springai-demo/src/test/java/scl/springaidemo/SpringaiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package scl.springaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
